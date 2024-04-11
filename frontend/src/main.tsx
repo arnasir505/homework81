2d790e3529b99f8c80315919ffa37b2daf98d5fc
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-11 17:15:34 +0600

    Configure redux store and create custom app hooks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { CssBaseline } from '@mui/material';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </Provider>
 );
