18e4847c4cb306945368989167e6a2ef6e489483
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-11 16:29:59 +0600

    Install mongoose, create config.ts, remove mongodb package

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import linksRouter from './routers/links';
-import mongoDb from './mongoDb';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
 const port = 8000;
@@ -12,14 +13,14 @@ app.use(express.json());
 app.use('/links', linksRouter);
 
 const run = async () => {
-  await mongoDb.connect();
+  await mongoose.connect(config.mongoose.db);
 
   app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
   });
 
   process.on('exit', () => {
-    mongoDb.disconnect();
+    mongoose.disconnect();
   });
 };
 
